14a99ced8f61eacf5af1a64b6efe19e11cda76b6
rai <ravaen1001@gmail.com>
2023-03-29 23:29:37 -0500

    creacion de mapa en html

--- mapa.py
+++ mapa.py
@@ -1,1 +1,12 @@
-print("inicio de proyecto")
+
+import folium
+import webbrowser
+
+# Crea un objeto mapa centrado en una ubicación específica
+mapa = folium.Map(location=[-14.048148, -75.748584], zoom_start=12)
+
+# Guarda el mapa en un archivo HTML
+mapa.save('mapa.html')
+
+# Abre el archivo HTML en el navegador predeterminado del usuario
+webbrowser.open('mapa.html')
