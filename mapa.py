1297675a8864b24bf87a03d29f3ce0ca4c381ebf
rai <ravaen1001@gmail.com>
2023-03-30 22:30:11 -0500

    salida con marcadores y mensaje

--- mapa.py
+++ mapa.py
@@ -3,7 +3,11 @@ import folium
 import webbrowser
 
 # Crea un objeto mapa centrado en una ubicación específica
-mapa = folium.Map(location=[-14.048148, -75.748584], zoom_start=12)
+mapa = folium.Map(location=[-14.048148, -75.739941], zoom_start=12)
+
+# Agrega un marcador en la misma ubicación
+folium.Marker(location=[-14.076831, -75.748412], popup="hablen perrxs nos vemos en el bruce!!").add_to(mapa)
+
 
 # Guarda el mapa en un archivo HTML
 mapa.save('mapa.html')
